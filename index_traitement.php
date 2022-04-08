5dc0beac20c483b4815e4d13b9cbfc802c9895ed
Lucas <lucas.dpt8@gmail.com>
2022-04-08 09:45:25 +0200

    je sais pas pq j'ai eu un changement mais j'ai pas le choix

--- index_traitement.php
+++ index_traitement.php
@@ -6,6 +6,7 @@ require_once "db_connect.php";
 
 if (!empty($_POST["prenom"]) && (!empty($_POST["nom"]))  && (!empty($_POST["email"]))  && (!empty($_POST["date"])) && (!empty($_POST["password"])) && (!empty($_POST["retype_password"]))) {
 
+
     $prenom = htmlspecialchars($_POST["prenom"]);
     $nom = htmlspecialchars($_POST["nom"]);
     $email = htmlspecialchars($_POST["email"]);
@@ -13,20 +14,35 @@ if (!empty($_POST["prenom"]) && (!empty($_POST["nom"]))  && (!empty($_POST["emai
     $retype_password = htmlspecialchars($_POST["retype_password"]);
     $date = htmlspecialchars($_POST["date"]);
 
-    if (($_POST["password"]) === ($_POST["retype_password"])) {
 
-        $cost = ['cost' => 12];
-        $password = password_hash($password, PASSWORD_BCRYPT, $cost);
 
-        $insert = $bdd->prepare("INSERT INTO `membres` (`prenom`, `nom`, `email`, `date`, `password`, `id`) VALUES (:prenom, :nom, :email, :date, :password, NULL)");
-        $insert->execute(array(
-            "prenom" => $prenom,
-            "nom" => $nom,
-            "email" => $email,
-            "password" => $password,
-            "date" => $date
-        ));
-    } else echo ("tu n'es pas connecter");
+    $check = $bdd->prepare("SELECT `prenom`, `email`, password FROM membres WHERE email = ?");
+    $check->execute(array($email));
+    $data = $check->fetch();
+    $row = $check->rowCount();
+
+    $email = strtolower($email);
+
+
+    if ($row == 0) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            if (($_POST["password"]) === ($_POST["retype_password"])) {
+
+                $cost = ['cost' => 12];
+                $password = password_hash($password, PASSWORD_BCRYPT, $cost);
+
+
+                $insert = $bdd->prepare("INSERT INTO `membres` (`prenom`, `nom`, `email`, `date`, `password`, `id`) VALUES (:prenom, :nom, :email, :date, :password, NULL)");
+                $insert->execute(array(
+                    "prenom" => $prenom,
+                    "nom" => $nom,
+                    "email" => $email,
+                    "password" => $password,
+                    "date" => $date
+                ));
+            } else echo ("tu n'es pas connecter");
+        } else header('location: index.php');
+    } else header('location: index.php');
 } else header('location: index.php');
 ?>
 
